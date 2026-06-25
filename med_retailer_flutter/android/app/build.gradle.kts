1410bd07d7a38c1e0894173480ee6309a9d6c787
MedRetailer Developer <dev@medretailer.local>
2026-06-25 16:15:08 +0800

    Set compileSdk to 35 to enforce targetSdk 35

--- med_retailer_flutter/android/app/build.gradle.kts
+++ med_retailer_flutter/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.pims.medretailers"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 35
     ndkVersion = flutter.ndkVersion
 
     compileOptions {
